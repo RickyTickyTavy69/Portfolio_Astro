a34636661b333fce73d03466cda6a3431ee7c1d3
Artem Bakirov <bakirovartem69@gmail.com>
2022-12-05 09:43:36 +0100

    added deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://RickyTickyTavy69.github.io",
+  base: "/Portfolio_Astro",
   integrations: [react()],
 });
